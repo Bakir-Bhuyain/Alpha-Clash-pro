1fc93a9e5ea16c98e6a636d41f8c8350337eaa11
Bakir Bhuyain <hemalbakir@gmail.com>
2025-08-13 00:16:52 +0600

    full game complete

--- script/alphaClash.js
+++ script/alphaClash.js
@@ -22,33 +22,56 @@ function handleKeyboardButtonPress (event){
       //check matched or not 
       if(playerPressed=== expectedAlphabet){
             console.log('you get a point ');
+            const currentScore = getTextElementValueById('current-score');
+            console.log(currentScore);
+            const updatedScore = currentScore + 1;
+
+            setTextElementById('current-score', updatedScore);
+
+
+
+            //------------------------------------------------
             //update score
             //1. get the correct score 
-            const currentScoreElement = document.getElementById("current-score");
-            const currentScoreText = currentScoreElement.innerText;
-            const currentScore = parseInt(currentScoreText);
-            console.log(currentScore);
-            //2. increase the score by one
-            const newScore = currentScore + 1 ;
-            //3.show the updated score
-            currentScoreElement.innerText = newScore;
+            // const currentScoreElement = document.getElementById("current-score");
+            // const currentScoreText = currentScoreElement.innerText;
+            // const currentScore = parseInt(currentScoreText);
+            // console.log(currentScore);
+            
+
+            // //2. increase the score by one
+            // const newScore = currentScore + 1 ;
+
+            // //3.show the updated score
+            // currentScoreElement.innerText = newScore;
 
 
 
-            //start a new round
+            // //start a new round
             removeBackgroundColorById(expectedAlphabet);
             continueGame();
       }else{
             console.log('you missed a life');
-            //step -1 : get the current life number
-            const currentLifeElement = document.getElementById('current-life');
-            const currentLifeText = currentLifeElement.innerText;
-            const currentLife = parseInt(currentLifeText);
 
-            //step -2 : reduce the life count 
-            const newLife = currentLife  -1;
-            //step-3 : display the updated life county
-            currentLifeElement.innerText = newLife;
+            const currentLife = getTextElementValueById('current-life');
+            const updatedLife = currentLife  - 1;
+            setTextElementById('current-life', updatedLife);
+
+            if (updatedLife === 0){
+                  gameOver();
+            }
+
+
+            //-------------------------------------------------
+            // //step -1 : get the current life number
+            // const currentLifeElement = document.getElementById('current-life');
+            // const currentLifeText = currentLifeElement.innerText;
+            // const currentLife = parseInt(currentLifeText);
+
+            // //step -2 : reduce the life count 
+            // const newLife = currentLife  -1;
+            // //step-3 : display the updated life county
+            // currentLifeElement.innerText = newLife;
             }
 }
 //capture keyboard key press
@@ -75,7 +98,33 @@ function continueGame(){
 
 
 function play(){
+      //hide everything show only play round 
       hideElementById('home-screen');
+      hideElementById('final-score');
       showElementById('play-ground');
+
+      //rest score and life 
+      setTextElementById('current-life',5);
+      setTextElementById('current-score',0);
+
+
       continueGame();
+}
+
+function gameOver (){
+      hideElementById('play-ground');
+      showElementById('final-score');
+      //update final score 
+      // 1. get the final score 
+      const lastScore = getTextElementValueById('current-score');
+      console.log(lastScore);
+      setTextElementById('last-score',lastScore);
+
+      //clear the last selected alphabet
+      const alphabet = getElementById('current-alphabet');
+
+      //clear the last selected alphabet highlight 
+      const currentAlphabet = getElementTextById('current-alphabet');
+      // console.log(currentAlphabet);
+      removeBackgroundColorById(currentAlphabet);
 }
